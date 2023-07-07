ad53ccf8c9a05ad8502599facf7b0c6ace7e99ab
Aziz Mlayel <119380982+AzizMlayel13@users.noreply.github.com>
2023-07-07 19:29:56 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <cal> </cal>
+  </div>
+</template>
+
+<script>
+import Cal from './components/Cal.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Cal
+  }
+}
+</script>
+
+<style >
+body {
+  background: #31475e !important;
+}
+</style>
